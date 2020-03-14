55698e34a24c3de85fe4d2fea89f96287714861a
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-03-14 02:43:36 -0500

    file structure created

---
+++ empdirectory/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Wrapper from './components/Wrapper.jsx'
+
+function App() {
+    return (
+        <div className="App">
+            <Wrapper>
+                
+            </Wrapper>
+        </div>
+    )
+}
+
+export default App;
--- empdirectory/src/utils/API.js
+++ empdirectory/src/utils/API.js
@@ -3,7 +3,9 @@ import axios from 'axios'
 export default {
     getUsers: () => {
         return axios.get(
-            `https://randomuser.me/api/?results=1000&nat=us&inc=name,email,dob,cell,picture`
+            `https://randomuser.me/api/?results=1000&nat=us`
         )
     }
 }
+
+// &inc=name,email,dob,cell,picture
