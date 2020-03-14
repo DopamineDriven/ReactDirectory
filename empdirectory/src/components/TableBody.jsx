c22ddc3e99196357ad089857ac0a55ea29a67c6b
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-03-14 03:29:56 -0500

    tableBody.jsx completed

--- empdirectory/src/components/TableBody.jsx
+++ empdirectory/src/components/TableBody.jsx
@@ -21,7 +21,48 @@ const TableBody = () => {
 
     return (
         <tbody>
-
+            {context.developerState.filteredUsers[0] !== undefined && context.developerState.filteredUsers[0].name !== undefined ? (
+                context.developerState.filteredUsers.map(({ login, name, picture, phone, email, dob }) => {
+                    return (
+                        // this api uses uuid for user-sensitive login info
+                        // this is confirmed via the use of postman to get the info
+                        // example: "login": {"uuid": "80627358-57b1-4106-ad03-166933a31e7a",...}
+                        // data-th is data for the table header corresponding to image
+                        <tr key={login.uuid}>
+                            <td className="align-middle" data-th="image">
+                                <img 
+                                    src={picture.medium} 
+                                    alt={`profile image for ${name.first} ${name.last}`} 
+                                    className="img-responsive"
+                                />
+                            </td>
+                            <td className="align-middle" data-th="name">
+                                {name.first} {name.last}
+                            </td>
+                            <td className="align-middle" data-th="phone">
+                                {phone}
+                            </td>
+                            <td className="align-middle" data-th="email">
+                                {email}
+                            </td>
+                            <td className="align-middle" data-th="dob">
+                                {FormatDOB(dob.date)}
+                            </td>
+                        </tr>
+                    )
+                })
+            ) : (
+                // empty else :( <></> ) to end ternary function
+                <></>
+            )}
         </tbody>
     )
 }
+
+export default TableBody;
+
+// consider using syntax to have email open in default mailing app
+// this can be achieved via 
+// <a href={`mailto: ${email}`} target='__blank'>
+//     {email}
+// </a>
